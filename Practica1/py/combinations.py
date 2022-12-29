eb5af5555f896e62ccd1aa3109386b66891176e7
Strunky25 <41077084+Strunky25@users.noreply.github.com>
2022-12-29 17:16:24 +0100

    langs no se

--- Practica1/py/combinations.py
+++ Practica1/py/combinations.py
@@ -4,60 +4,63 @@ import wordfreq
 
 # Returns the number of appearances of any char from string in word.
 def count_special_characters(word: str, string: str) -> int:
-    num_special_characters=0
+    num_special_characters = 0
     for char in word:
         if char in string:
-            num_special_characters+=1
+            num_special_characters += 1
     return num_special_characters
 
 # Function that creates all the pairs of 2 vowels.
+
+
 def diptongos() -> list:
-    list=[]
+    list = []
     vowels = "aeiouàèìòùáéíóúäëïöüâêîôû"
     for i in vowels:
         for j in vowels:
             list.append(i+j)
     return list
 
+
 # Length
 dict = {
-        'vow':'aeiou', 
-        'acc':'àèìòùáéíóú', 
-        'accl':'àèìòù', 
-        'accr':'áéíóú', 
-        'die':'äëïöü',
-        'cir':'âêîôû', 
-        'ñ':'ñ', 
-        'ç':'ç', 
-        'ale': 'ß', 
-        'rus': 'бвгджзийклмнпрстфцчшщъыьэюя',
-        'pol': 'ąćęłńóśźż', 
-        'por': 'ãõ', 
-        'sue': 'åäö',
-        'esp': 'áéíóúü',
-        'ita': 'àèéìíîòóùú',
-        'fra': 'àâæçéèêëîïôœùûüÿ',
-        'ger': 'äöüß',
-        'cat': 'àèéíïòóúüç',
-        'num_words': ' ',
-        "apos": "'",
-        "hyph": "-",
-        "rares": "kqwxyz"
+    'vow': 'aeiou',
+    'acc': 'àèìòùáéíóú',
+    'accl': 'àèìòù',
+    'accr': 'áéíóú',
+    'die': 'äëïöü',
+    'cir': 'âêîôû',
+    'ñ': 'ñ',
+    'ç': 'ç',
+    'ale': 'ß',
+    'rus': 'бвгджзийклмнпрстфцчшщъыьэюя',
+    'pol': 'ąćęłńóśźż',
+    'por': 'ãõ',
+    'sue': 'åäö',
+    'esp': 'áéíóúü',
+    'ita': 'àèéìíîòóùú',
+    'fra': 'àâæçéèêëîïôœùûüÿ',
+    'ger': 'äöüß',
+    'cat': 'àèéíïòóúüç',
+    'num_words': ' ',
+    "apos": "'",
+    "hyph": "-",
+    "rares": "kqwxyz"
 }
 
 # Dictionary containing feature name, and list of the leetter groups in that language.
 groups = {
-        "pairs_eng": ["sh", "th", "ch", "ck", "ph", "ng", "qu", "tr", "st", "wh", "tr"],
-        "pairs_cat": ["ny", "tx", "sc", "nc", "rc", "ll", "nc", "pc", "pr", "br", "fr", "ts", "ix",  "nd", "pr", "bl"],
-        "pairs_esp": ["nd", "nt", "ch", "rr", "ll", "qu", "gu", "nc", "mb", "pr"],
-        "pairs_ger": ["tch", "ck", "ng", "qu", "tz", "ss", "st", "sp", "str", "sch"],
-        "pairs_por": ["tch", "lh", "nh", "qu", "sc", "rr", "nc", "gu", "lm", "rm"],
-        "pairs_pol": ["ch", "dz", "dł", "di", "rz", "sz", " sc", "ed", "id"],
-        "pairs_ita": ["ch", "gl", "gn", "sc", "qu", "scl", "ch", "ci", "gli", "gn", "io", "la", "leu", "ii", "io", "ne"],
-        "pairs_swe": ["ch", "ck", "cid", "dt", "gg", "ll", "ng", "sk", "st", "tt"],
-        "pairs_fre": ["ch", "che", "eau", "ent", "es", "ette", "eur", "iau", "ie", "in"],
-        "pairs_rus": ["бл", "вл", "гл", "дл", "жл", "зл", "кл", "лл", "мл", "нл", "пл", "рл", "сл", "тл", "фл", "хл", "цл", "чл", "шл", "щл"],
-        "diptongos": diptongos()
+    "pairs_eng": ["sh", "th", "ch", "ck", "ph", "ng", "qu", "tr", "st", "wh", "tr"],
+    "pairs_cat": ["ny", "tx", "sc", "nc", "rc", "ll", "nc", "pc", "pr", "br", "fr", "ts", "ix",  "nd", "pr", "bl"],
+    "pairs_esp": ["nd", "nt", "ch", "rr", "ll", "qu", "gu", "nc", "mb", "pr"],
+    "pairs_ger": ["tch", "ck", "ng", "qu", "tz", "ss", "st", "sp", "str", "sch"],
+    "pairs_por": ["tch", "lh", "nh", "qu", "sc", "rr", "nc", "gu", "lm", "rm"],
+    "pairs_pol": ["ch", "dz", "dł", "di", "rz", "sz", " sc", "ed", "id"],
+    "pairs_ita": ["ch", "gl", "gn", "sc", "qu", "scl", "ch", "ci", "gli", "gn", "io", "la", "leu", "ii", "io", "ne"],
+    "pairs_swe": ["ch", "ck", "cid", "dt", "gg", "ll", "ng", "sk", "st", "tt"],
+    "pairs_fre": ["ch", "che", "eau", "ent", "es", "ette", "eur", "iau", "ie", "in"],
+    "pairs_rus": ["бл", "вл", "гл", "дл", "жл", "зл", "кл", "лл", "мл", "нл", "пл", "рл", "сл", "тл", "фл", "хл", "цл", "чл", "шл", "щл"],
+    "diptongos": diptongos()
 }
 
 # Dictionary containing feature name, and list of the common prefixes in that language.
@@ -88,10 +91,11 @@ common_suffixes = {
     'suf_rus': ["больше", "енький", "ик", "ичка", "ок", "онок", "ушко", "ца", "чек", "шка", "шко", "ящик", "ец", "ин", "ист", "ник", "овец", "щик", "ёнок", "ь"]
 }
 
-language_codes = ['en', 'es', 'ca', 'it', 'fr', 'pt', 'de', 'sv', 'pl', 'ru']
-
+language_codes = ['en', 'de', 'ca', 'es', 'fr', 'it', 'pl', 'pt', 'ru', 'sv']
 
 # Returns if word starts with a prefix from prefixes list.
+
+
 def has_prefix(word: str, prefixes: list) -> int:
     for prefix in prefixes:
         if word.startswith(prefix):
@@ -99,6 +103,8 @@ def has_prefix(word: str, prefixes: list) -> int:
     return 0
 
 # Returns if word ends with a prefix from suffixes list.
+
+
 def has_suffix(word: str, suffixes: list) -> int:
     for suffix in suffixes:
         if word.endswith(suffix):
@@ -107,44 +113,52 @@ def has_suffix(word: str, suffixes: list) -> int:
 
 
 def count_group(word: str, groups: list) -> int:
-    num_groups=0
+    num_groups = 0
     for group in groups:
         if group in word:
-            num_groups+=1
+            num_groups += 1
     return num_groups
 
 
 def A(df_AUX):
     df_AUX['len'] = df_AUX['word'].str.len()
     for column in dict:
-        df_AUX[column] = df_AUX['word'].apply(lambda row: count_special_characters(row, dict[column]))
+        df_AUX[column] = df_AUX['word'].apply(
+            lambda row: count_special_characters(row, dict[column]))
     return df_AUX
+
 
 def B(df_AUX):
     # For every entry in the dictionary we create that column using the function above.
     for column in groups:
-            df_AUX[column] = df_AUX['word'].apply(lambda row: count_group(row, groups[column]))
+        df_AUX[column] = df_AUX['word'].apply(
+            lambda row: count_group(row, groups[column]))
     return df_AUX
+
 
 def C(df_AUX):
     # For every entry in the dictionary we create that column using the function above.
     for column in common_prefixes:
-            df_AUX[column] = df_AUX['word'].apply(lambda row: has_prefix(row, common_prefixes[column]))
+        df_AUX[column] = df_AUX['word'].apply(
+            lambda row: has_prefix(row, common_prefixes[column]))
     # For every entry in the dictionary we create that column using the function above.
     for column in common_suffixes:
-        df_AUX[column] = df_AUX['word'].apply(lambda row: has_suffix(row, common_suffixes[column]))
+        df_AUX[column] = df_AUX['word'].apply(
+            lambda row: has_suffix(row, common_suffixes[column]))
 
     return df_AUX
+
 
 def D(df_AUX):
 
     for code in language_codes:
-            df_AUX[code+'_zipf'] = df_AUX['word'].apply(lambda row: wordfreq.zipf_frequency(row, code))
+        df_AUX[code+'_zipf'] = df_AUX['word'].apply(
+            lambda row: wordfreq.zipf_frequency(row, code))
     return df_AUX
 
 
 # Read Data and convert to lowercase strings
-df = pd.read_csv('data/data3.csv', sep=',')
+df = pd.read_csv('data/data.csv', sep=',')
 df = df.apply(lambda x: x.astype(str).str.lower())
 
 # concat all languages into one Dataframe
@@ -160,18 +174,18 @@ df.head()
 df['lang'] = df['lang'].astype('category').cat.codes
 
 
-combinations = [("A", A), ("B", B), ("C", C), ("D", D), 
-                ("A", A, "B", B), ("A", A, "C", C), ("A", A, "D", D), 
-                ("B", B, "C", C), ("B", B, "D", D), ("C", C, "D", D), 
-                ("A", A, "B", B, "C", C), ("A", A, "B", B, "D", D), 
-                ("A", A, "C", C, "D", D), ("B", B, "C", C, "D", D), 
+combinations = [("A", A), ("B", B), ("C", C), ("D", D),
+                ("A", A, "B", B), ("A", A, "C", C), ("A", A, "D", D),
+                ("B", B, "C", C), ("B", B, "D", D), ("C", C, "D", D),
+                ("A", A, "B", B, "C", C), ("A", A, "B", B, "D", D),
+                ("A", A, "C", C, "D", D), ("B", B, "C", C, "D", D),
                 ("A", A, "B", B, "C", C, "D", D)]
 
 # Call the functions for each combination
 for combination in combinations:
     df2 = df.copy()
     str = ""
-    for i in range(0, len(combination), 2):        
+    for i in range(0, len(combination), 2):
         df2 = combination[i+1](df2)
         str += combination[i]
     print(str)
--- Practica1/py/models_comb.py
+++ Practica1/py/models_comb.py
@@ -13,17 +13,17 @@ from sklearn.metrics import confusion_matrix
 import os
 
 
-
 # ### Read Preprocessed Data
 
 # for every file in data/comb open the csv
 # and append it to a list
-scaler = StandardScaler() # try different scalers
-pca = PCA(n_components=0.99, random_state=43) # try different n_components
+scaler = StandardScaler()  # try different scalers
+pca = PCA(n_components=0.99, random_state=43)  # try different n_components
 # Create common Models with different Kernels
 models = [
     svm.SVC(kernel='linear', probability=True),  # linear kernel
-    svm.SVC(kernel='poly', probability=True),  # polynomial kernel with degree 3
+    # polynomial kernel with degree 3
+    svm.SVC(kernel='poly', probability=True),
     svm.SVC(kernel='rbf', probability=True),  # RBF kernel with gamma = 0.1
     svm.SVC(kernel='sigmoid', probability=True)
 ]
@@ -35,8 +35,10 @@ for file in os.listdir('data/comb'):
     X = df.drop(['word', 'lang'], axis=1)
     y = df['lang']
     # Split data for training and testing
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=43) 
-    X_test, X_val, y_test, y_val = train_test_split(X_test, y_test, test_size=0.5, random_state=43)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=43)
+    X_test, X_val, y_test, y_val = train_test_split(
+        X_test, y_test, test_size=0.5, random_state=43)
 
     # Scale Data: https://towardsdatascience.com/feature-scaling-and-normalisation-in-a-nutshell-5319af86f89b
     X_train = scaler.fit_transform(X_train)
@@ -52,8 +54,11 @@ for file in os.listdir('data/comb'):
 
     for i, model in enumerate(models):
         prediction = model.predict_proba(X_test_pca)
-        print(f'Top 3 accuracy model {i+1}: {round(top_k_accuracy_score(y_test, prediction, k=3)*100,2)}%')
+        print(
+            f'Top 3 accuracy model {i+1}: {round(top_k_accuracy_score(y_test, prediction, k=3)*100,2)}%')
         # print accuracy, precision, f1-score
-        langs = ['en', 'ca', 'es', 'de', 'fr', 'pl', 'pt', 'ru', 'it', 'sv']
-        report = classification_report(y_test, model.predict(X_test_pca), target_names=langs, output_dict=True)
-        print("Accuracy: ", round(report['accuracy']*100, 2), "Precision: ", round(report['macro avg']['precision']*100,2), "F1-score: ", round(report['macro avg']['f1-score']*100,2))
+        langs = ['en', 'de', 'ca', 'es', 'fr', 'it', 'pl', 'pt', 'ru', 'sv']
+        report = classification_report(y_test, model.predict(
+            X_test_pca), target_names=langs, output_dict=True)
+        print("Accuracy: ", round(report['accuracy']*100, 2), "Precision: ", round(
+            report['macro avg']['precision']*100, 2), "F1-score: ", round(report['macro avg']['f1-score']*100, 2))
